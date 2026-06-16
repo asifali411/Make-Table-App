3bce4a5320c7024432b6c6629fd61a8154491973
geo-777 <geo.binu.25@gmail.com>
2026-06-16 22:52:29 +0530

    feat: started with dashbpard/

--- src/app/routes/RouteManager.tsx
+++ src/app/routes/RouteManager.tsx
@@ -3,37 +3,40 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "../../shared/components/navbar/Navbar";
 
 //Auths
-const Login    = lazy(() => import("../../features/auth/pages/Login"));
+const Login = lazy(() => import("../../features/auth/pages/Login"));
 const Register = lazy(() => import("../../features/auth/pages/Register"));
 
-const Classes     = lazy(() => import("../../features/classes/pages/Classes"));
-const Subjects    = lazy(() => import("../../features/subjects/pages/Subjects"));
-const Teachers    = lazy(() => import("../../features/teachers/pages/Teachers"));
-const Assignments = lazy(() => import("../../features/assignments/pages/Assignments"));
+const Dashboard = lazy(
+  () => import("../../features/dashboard/pages/Dashboard"),
+);
+const Classes = lazy(() => import("../../features/classes/pages/Classes"));
+const Subjects = lazy(() => import("../../features/subjects/pages/Subjects"));
+const Teachers = lazy(() => import("../../features/teachers/pages/Teachers"));
+const Assignments = lazy(
+  () => import("../../features/assignments/pages/Assignments"),
+);
 
 // Placeholders
-const Loading        = () => <div>LOADING</div>;
-const Dashboard      = () => <div>DASHBOARD</div>;
-const Settings       = () => <div>SETTINGS</div>;
+const Loading = () => <div>LOADING</div>;
+const Settings = () => <div>SETTINGS</div>;
 const HelpAndSupport = () => <div>HELP AND SUPPORT</div>;
 
 const RouteManager = () => {
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
-
-        <Route path="/login"    element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
+
         <Route element={<Navbar />}>
           <Route path="/" element={<Loading />}></Route>
-          <Route path="/dashboard"   element={<Dashboard />      }></Route>
-          <Route path="/classes"     element={<Classes />        }></Route>
-          <Route path="/subjects"    element={<Subjects />       }></Route>
-          <Route path="/teachers"    element={<Teachers />       }></Route>
-          <Route path="/assignments" element={<Assignments />    }></Route>
-          <Route path="/settings"    element={<Settings />       }></Route>
-          <Route path="/help"        element={<HelpAndSupport /> }></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/classes" element={<Classes />}></Route>
+          <Route path="/subjects" element={<Subjects />}></Route>
+          <Route path="/teachers" element={<Teachers />}></Route>
+          <Route path="/assignments" element={<Assignments />}></Route>
+          <Route path="/settings" element={<Settings />}></Route>
+          <Route path="/help" element={<HelpAndSupport />}></Route>
         </Route>
       </Routes>
     </Suspense>
---
+++ src/features/dashboard/pages/Dashboard.tsx
@@ -0,0 +1,25 @@
+import styles from "../styles/Dashboard.module.css";
+import { Plus } from "lucide-react";
+const Dashboard = () => {
+  return (
+    <div>
+      <header className={styles.header}>
+        <div>
+          <h1 className={styles.title}>Dashboard</h1>
+          <p className={styles.subtitle}>Create timetables</p>
+        </div>
+
+        <button
+          type="button"
+          className={styles.primaryButton}
+          onClick={() => {}}
+        >
+          <Plus size={16} strokeWidth={2} />
+          Create Timetable
+        </button>
+      </header>
+    </div>
+  );
+};
+
+export default Dashboard;
